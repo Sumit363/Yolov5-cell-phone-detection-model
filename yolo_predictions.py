a470d610393744bd46859bf43314ac60c7ccbdff
Sumit Basnet <103460051+Sumit363@users.noreply.github.com>
2026-04-04 21:08:01 -0500

    Update yolo_predictions.py

--- yolo_predictions.py
+++ yolo_predictions.py
@@ -91,10 +91,10 @@ class YOLO_Pred():
 
             text = f'{class_name}: {bb_conf}%'
 
-            cv2.rectangle(image,(x,y),(x+w,y+h),colors,4)
+            cv2.rectangle(image,(x,y),(x+w,y+h),colors,2)
             cv2.rectangle(image,(x,y-30),(x+w,y),colors,-1)
 
-            cv2.putText(image,text,(x,y-10),cv2.FONT_HERSHEY_PLAIN,0.7,(0,0,0),4)
+            cv2.putText(image,text,(x,y-10),cv2.FONT_HERSHEY_PLAIN,0.7,(0,0,0),2)
             
             
         return image
